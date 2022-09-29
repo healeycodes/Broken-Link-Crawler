b1c8417b12122786eb8c2072308c07ff1594e9db
Stathis Xantinidis <spithash@tuta.io>
2022-09-29 22:55:46 +0300

    Update deadseeker.py

--- deadseeker.py
+++ deadseeker.py
@@ -1,7 +1,7 @@
-'''
+"""
 deadseeker.py
 Seeking out your 404s in around 50 lines of vanilla Python.
-'''
+"""
 
 import sys
 import urllib.request, urllib.parse, urllib.error
@@ -11,14 +11,14 @@ from urllib.request import Request
 from html.parser import HTMLParser
 from collections import deque
 
-search_attrs = set(['href', 'src'])
-agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
+search_attrs = set(["href", "src"])
+agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
 
 
 class LinkParser(HTMLParser):
     def __init__(self, home, verbose):
-        ''':home:    a homepage, e.g. 'https://healeycodes.com/'
-           :verbose: boolean for for verbose mode'''
+        """:home:    a homepage, e.g. 'https://healeycodes.com/'
+        :verbose: boolean for for verbose mode"""
         super().__init__()
         self.home = home
         self.verbose = verbose
@@ -28,45 +28,45 @@ class LinkParser(HTMLParser):
         self.scanner()
 
     def scanner(self):
-        '''Loop through remaining pages, looking for HTML responses'''
+        """Loop through remaining pages, looking for HTML responses"""
         while self.pages_to_check:
             page = self.pages_to_check.pop()
-            req = Request(page, headers={'User-Agent': agent})
+            req = Request(page, headers={"User-Agent": agent})
             res = request.urlopen(req)
-            if 'html' in res.headers['content-type']:
+            if "html" in res.headers["content-type"]:
                 with res as f:
-                    body = f.read().decode('utf-8', errors='ignore')
+                    body = f.read().decode("utf-8", errors="ignore")
                     self.feed(body)
 
     def handle_starttag(self, tag, attrs):
-        '''Override parent method and check tag for our attributes'''
+        """Override parent method and check tag for our attributes"""
         for attr in attrs:
             # ('href', 'http://google.com')
             if attr[0] in search_attrs and attr[1] not in self.checked_links:
                 self.checked_links.add(attr[1])
                 self.handle_link(attr[1])
-                
+
     def handle_link(self, link):
-        '''Send a HEAD request to the link, catch any pesky errors'''
+        """Send a HEAD request to the link, catch any pesky errors"""
         if not bool(urlparse(link).netloc):  # relative link?
             link = urljoin(self.home, link)
         try:
-            req = Request(link, headers={'User-Agent': agent}, method='HEAD')
+            req = Request(link, headers={"User-Agent": agent}, method="HEAD")
             status = request.urlopen(req).getcode()
         except urllib.error.HTTPError as e:
-            print(f'HTTPError: {e.code} - {link}')  # (e.g. 404, 501, etc)
+            print(f"HTTPError: {e.code} - {link}")  # (e.g. 404, 501, etc)
         except urllib.error.URLError as e:
-            print(f'URLError: {e.reason} - {link}')  # (e.g. conn. refused)
+            print(f"URLError: {e.reason} - {link}")  # (e.g. conn. refused)
         except ValueError as e:
-            print(f'ValueError {e} - {link}')  # (e.g. missing protocol http)
+            print(f"ValueError {e} - {link}")  # (e.g. missing protocol http)
         else:
             if self.verbose:
-                print(f'{status} - {link}')
+                print(f"{status} - {link}")
         if self.home in link:
             self.pages_to_check.appendleft(link)
 
 
 # check for verbose tag
-verbose = len(sys.argv) > 2 and sys.argv[2] == 'v'
+verbose = len(sys.argv) > 2 and sys.argv[2] == "v"
 # enable this as a script, e.g., 'https://healeycodes.com/ v'
 LinkParser(sys.argv[1], verbose)
